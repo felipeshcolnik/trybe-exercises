18b331888eacc160a3384a7b8e100ce5fafbd301
Felipe Shcolnik <shcolnik@gmail.com.br>
2020-11-17 13:10:30 -0300

    arquivo da 2 alteracao com mais linhas

---
+++ readme2.js
@@ -0,0 +1,7 @@
+let pessoaEstudante = "euMesmo";
+let habilidadesPessoaEstudante = 10;
+// eu sou tryber e vou melhorar o mundo
+// Teste para envio a GItHub
+// Estudar faz bem"
+
+console.log("sucesso");
