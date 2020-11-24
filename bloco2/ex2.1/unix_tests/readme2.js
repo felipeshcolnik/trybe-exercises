16590bc7a6f8340ff8ad09b63db1a5818e2a2b83
Felipe Shcolnik <shcolnik@gmail.com.br>
2020-11-24 00:16:15 -0300

    reorganizacao de arquivos

---
+++ bloco2/ex2.1/unix_tests/readme2.js
@@ -0,0 +1,10 @@
+
+let pessoaEstudante = "euMesmo";
+let habilidadesPessoaEstudante = 10;
+// eu sou tryber e vou melhorar o mundo
+// Teste para envio a GItHub
+// Estudar faz bem"
+/// Vou ter muito sucesso na programação!
+
+console.log("Sucesso!");
+
