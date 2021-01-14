c93b769ebc013f6093cc58f2ae04f6a83c739a0d
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-01-14 15:51:44 -0300

    exercicio7,2 Parte 2 completo

--- Introducao/bloco7/d2/exercicio7_2Parte2.js
+++ Introducao/bloco7/d2/exercicio7_2Parte2.js
@@ -21,29 +21,29 @@ const lesson3 = {
 // APLICAÇÃO
 
 // exercicio1 Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela
-// function addKey (obj, key, value) { 
-//   obj[key] = value;
-//   console.log(obj);
-// }
-// addKey(lesson2, 'turno', 'manha');
+function addKey (obj, key, value) { 
+  obj[key] = value;
+  console.log(obj);
+}
+addKey(lesson2, 'turno', 'manha');
 
 // Exercicio 2 Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
 
-// function ListKeys(obj) { 
-//   return Object.keys(obj);
-// } // OU PODEMOS USAR A ARROW FUNCTION >> ListKeys = (obj) => Object.keys(obj);
+function ListKeys(obj) { 
+  return Object.keys(obj);
+} // OU PODEMOS USAR A ARROW FUNCTION >> ListKeys = (obj) => Object.keys(obj);
 
-// console.log(ListKeys(lesson1));
+console.log(ListKeys(lesson1));
 
 // Exercicio 3 Crie uma função para mostrar o tamanho de um objeto.
 
-// ObjectSize = (obj) => Object.keys(obj).length;
-// console.log(ObjectSize(lesson3));
+ObjectSize = (obj) => Object.keys(obj).length;
+console.log(ObjectSize(lesson3));
 
 // Exercicio 4 Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
 
-// ObjectValues = (obj) => Object.values(obj);
-// console.log(ObjectValues(lesson2));
+ObjectValues = (obj) => Object.values(obj);
+console.log(ObjectValues(lesson2));
 
 // Exercicio 5  Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
 
@@ -55,7 +55,7 @@ allLessons.lesson4 = {
   numeroEstudantes: 5
 };
 
-// Exercicio 6 
+// Exercicio 6 Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
 
 // OPCAO 1
 console.log((allLessons.lesson1.numeroEstudantes) + (allLessons.lesson2.numeroEstudantes) +(allLessons.lesson3.numeroEstudantes));
@@ -69,3 +69,23 @@ function teste (object) {
   return counter;
 }
 console.log(teste(allLessons));
+
+// EXERCICIO 7 // Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto
+function exerc7 (object, position) {
+  const array = Object.values(object);
+  return array[position];
+  }
+console.log(exerc7(allLessons,2));
+
+// EXERCICIO 8 // Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave. 
+
+function validadeObject (object, key, value) { 
+  const arr = Object.entries(object);
+  let isEqual = false;
+  for (let i in arr) {
+    if (arr[i][0] === key && arr[i][1] === value) isEqual = true;
+  }  
+  return isEqual;
+}
+
+console.log(validadeObject(lesson1, 'professor', 'Maria Clara'));
