f19fa2b03ca32919c4fe7cd35502e66eb1c81c28
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-01-22 14:00:10 -0300

    exercicio 2

--- Introducao/bloco8/dia4/exercise2.js
+++ Introducao/bloco8/dia4/exercise2.js
@@ -1,9 +1,16 @@
 const assert = require('assert')
 
 // escreva sum abaixo
-const (sum) => () {
 
-}
+// let numbers1 = [1,2,3,4];
+const sum = (...numbers) => {
+    let total = 0
+    for (let index = 0; index < numbers.length; index += 1) {
+        total += numbers[index]
+      }
+      return total;
+     }
+
 
 assert.strictEqual(sum(), 0)
 assert.strictEqual(sum(1), 1)
