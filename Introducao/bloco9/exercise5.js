868db68676f1ecda4c4b23e6c0edbd5b62486d89
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-01-28 13:34:55 -0300

    exercise5 OK

--- Introducao/bloco9/exercise5.js
+++ Introducao/bloco9/exercise5.js
@@ -4,22 +4,22 @@ const getMarsTemperature = () => {
   const maxTemperature = 58;
   return Math.floor(Math.random() * maxTemperature);
 };
-const degreeCelsius = getMarsTemperature(); // Felipe's code
-console.log(degreeCelsius);
+const Celsius = getMarsTemperature(); // Felipe's code
+// console.log(Celsius);
 
 const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
-console.log((degreeCelsius) => (degreeCelsius * 9/5) + 32);
+// console.log(toFahrenheit(Celsius));
 const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
-console.log(temperatureInFahrenheit);
+// console.log(temperatureInFahrenheit(toFahrenheit(Celsius)));
 const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
-console.log(greet);
+const gretting = (greet(toFahrenheit(Celsius)));
 
 // definição da função sendMarsTemperature...
-const temperature = toFahrenheit(degreeCelsius); // Felipe's code
+const temperature = toFahrenheit(Celsius); // Felipe's code
 
-const sendMarsTemperature = (temperature, greeting) => {
-  
-  setTimeout((temperatureInFahrenheit, greet), messageDelay()); 
-}
-sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
-sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
+const sendMarsTemperature = (greeting) => {
+  setTimeout(greeting, messageDelay()); 
+}  
+
+// sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
+// sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
