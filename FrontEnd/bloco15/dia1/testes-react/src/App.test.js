8e883dc7733da538663ecaf82c2ec621124aa63e
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-03-22 14:03:24 -0300

    deleting repository folder

--- FrontEnd/bloco15/dia1/testes-react/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  const meuApp = render(<App />);
-  const linkElement = meuApp.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
